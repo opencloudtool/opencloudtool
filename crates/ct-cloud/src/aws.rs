5785ccc66293a1dd75cee1f9e91a2264ab3bf852
Vladimir Minev <minev.dev@gmail.com>
2024-10-26 12:50:40 -0700

    Add key pair and user data to aws ec2 instance creation

--- crates/ct-cloud/src/aws.rs
+++ crates/ct-cloud/src/aws.rs
@@ -1,6 +1,9 @@
 use aws_sdk_ec2::config;
 use aws_sdk_ec2::types;
 use aws_sdk_ec2::Client;
+use base64::{engine::general_purpose, Engine as _};
+use std::process::Command;
+use uuid;
 
 pub async fn create_ec2_instance() -> Result<String, Box<dyn std::error::Error>> {
     // Load AWS configuration
@@ -9,17 +12,39 @@ pub async fn create_ec2_instance() -> Result<String, Box<dyn std::error::Error>>
         .region(region_provider)
         .load()
         .await;
-    let client = Client::new(&config);
+    let client: Client = Client::new(&config);
 
     // Specify instance details
     let instance_type = "t2.micro";
     let ami_id = "ami-0c65adc9a5c1b5d7c";
+
+    // Generate ssh key pair
+    let key_pair = client
+        .create_key_pair()
+        .key_name(uuid::Uuid::new_v4().to_string())
+        .send()
+        .await?;
+
+    // User data
+    let user_data = r#"#!/bin/bash
+set -e
+
+echo 'Hello, world!' > /home/ubuntu/hello.txt
+echo 'This is a multiline user data script' >> /home/ubuntu/hello.txt
+apt-get update
+apt-get install -y nginx
+systemctl start nginx
+"#;
+
+    let encoded_user_data = general_purpose::STANDARD.encode(user_data);
 
     // Launch EC2 instance
     let response = client
         .run_instances()
         .instance_type(types::InstanceType::from(instance_type))
         .image_id(ami_id)
+        .user_data(encoded_user_data)
+        .key_name(key_pair.key_name().unwrap())
         .min_count(1)
         .max_count(1)
         .send()
@@ -33,7 +58,18 @@ pub async fn create_ec2_instance() -> Result<String, Box<dyn std::error::Error>>
 
     // Return the instance ID or an error message
     match instance_id {
-        Some(id) => Ok(id),
+        Some(id) => {
+            // Save key pair to file
+            let key_pair_file_path = format!("{}.pem", id);
+            std::fs::write(&key_pair_file_path, key_pair.key_material().unwrap())?;
+
+            Command::new("chmod")
+                .arg("400")
+                .arg(key_pair_file_path)
+                .spawn()?;
+
+            Ok(id)
+        }
         None => Err("Failed to launch EC2 instance".into()),
     }
 }
@@ -47,12 +83,33 @@ pub async fn destroy_ec2_instance(instance_id: &str) -> Result<(), Box<dyn std::
         .await;
     let client = Client::new(&config);
 
+    // Get instance details
+    let instance_details = client
+        .describe_instances()
+        .instance_ids(instance_id)
+        .send()
+        .await?;
+
+    // Extract key name from instance details
+    let key_name = instance_details
+        .reservations()
+        .first()
+        .and_then(|r| r.instances().first())
+        .and_then(|i| i.key_name())
+        .ok_or("Failed to get key name")?;
+
     // Terminate the instance
     client
         .terminate_instances()
         .instance_ids(instance_id)
         .send()
         .await?;
+
+    // Delete the key pair
+    client.delete_key_pair().key_name(key_name).send().await?;
+
+    // Delete key pair file
+    std::fs::remove_file(format!("{}.pem", instance_id))?;
 
     Ok(())
 }
