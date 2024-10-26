74a08dd027bb24b42c141d4deb1c5bcaa92d8d15
Vladimir Minev <minev.dev@gmail.com>
2024-10-25 17:35:38 -0700

    Add state file to `deploy` and `destroy` commands
    - Add `--state-file-path` arg

--- crates/ct-cli/src/main.rs
+++ crates/ct-cli/src/main.rs
@@ -1,5 +1,6 @@
 use clap::{Parser, Subcommand};
 use ct_cloud::create_ec2_instance;
+use serde_derive::{Deserialize, Serialize};
 
 #[derive(Parser)]
 #[clap(author, version, about, long_about = None)]
@@ -11,9 +12,21 @@ struct Cli {
 #[derive(Subcommand)]
 enum Commands {
     /// Deploy the application
-    Deploy,
+    Deploy(CommandArgs),
     /// Destroy the application
-    Destroy,
+    Destroy(CommandArgs),
+}
+
+#[derive(Parser)]
+struct CommandArgs {
+    /// Path to the state file
+    #[clap(long, default_value = "./state.json")]
+    state_file_path: String,
+}
+
+#[derive(Serialize, Deserialize)]
+struct State {
+    instance_id: String,
 }
 
 #[tokio::main]
@@ -21,13 +34,21 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let cli = Cli::parse();
 
     match &cli.command {
-        Commands::Deploy => {
+        Commands::Deploy(args) => {
             let instance_id = create_ec2_instance().await?;
             println!("Instance ID: {}", instance_id);
+
+            let state = State { instance_id };
+            let state_json = serde_json::to_string(&state)?;
+            std::fs::write(&args.state_file_path, state_json)?;
+
+            println!("State saved to {}", args.state_file_path);
         }
-        Commands::Destroy => {
-            println!("Destroying the application...");
-            // Add destruction logic here
+        Commands::Destroy(args) => {
+            let state = std::fs::read_to_string(&args.state_file_path)?;
+            let state: State = serde_json::from_str(&state)?;
+
+            println!("Destroying instance: {}", state.instance_id);
         }
     }
 
@@ -36,6 +57,8 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
 
 #[cfg(test)]
 mod tests {
+    use super::State;
+
     use assert_cmd::Command;
     use predicates::prelude::*;
     use std::env;
@@ -46,8 +69,13 @@ mod tests {
             env::set_var("AWS_ENDPOINT_URL", "http://localhost:4566");
         }
 
+        let temp_dir = tempfile::tempdir().unwrap();
+        let state_file = temp_dir.path().join("state.json");
+
         let mut cmd = Command::cargo_bin("ct-cli").unwrap();
         cmd.arg("deploy")
+            .arg("--state-file-path")
+            .arg(state_file.to_str().unwrap())
             .assert()
             .success()
             .stdout(predicate::str::contains("Instance ID:"));
@@ -55,11 +83,25 @@ mod tests {
 
     #[test]
     fn test_destroy_command() {
+        let instance_id = "i-0102f03dc608749c3";
+
+        let temp_dir = tempfile::tempdir().unwrap();
+        let state_file = temp_dir.path().join("state.json");
+
+        let state = State {
+            instance_id: String::from(instance_id),
+        };
+        let state_json = serde_json::to_string(&state).unwrap();
+
+        std::fs::write(&state_file, state_json).unwrap();
+
         let mut cmd = Command::cargo_bin("ct-cli").unwrap();
         cmd.arg("destroy")
+            .arg("--state-file-path")
+            .arg(state_file.to_str().unwrap())
             .assert()
             .success()
-            .stdout("Destroying the application...\n");
+            .stdout(format!("Destroying instance: {}\n", instance_id));
     }
 
     #[test]
