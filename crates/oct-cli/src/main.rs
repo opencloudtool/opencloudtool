c3a912d5e6ff42db60ff7cc3c2e99d3396198ae9
Andrei Agaev <andagaev@gmail.com>
2024-12-07 21:54:53 -0800

    Implement state managment v0.
    - Save resource info to state.json after deployment
    - Read from state.json resource info
    - Remove state.json file after resource destroyed

--- crates/oct-cli/src/main.rs
+++ crates/oct-cli/src/main.rs
@@ -1,8 +1,9 @@
+use std::fs;
+
 use clap::{Parser, Subcommand};
 use log;
 use oct_cloud::aws;
 use oct_cloud::aws::Resource;
-use oct_cloud::aws::State;
 
 #[derive(Parser)]
 #[clap(author, version, about, long_about = None)]
@@ -40,46 +41,38 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     let cli = Cli::parse();
 
+    let state_file_path = "./state.json";
+
     match &cli.command {
         Commands::Deploy(args) => {
             // Create EC2 instance
             let mut instance = aws::Ec2Instance::new(
                 "us-west-2".to_string(),
                 "ami-04dd23e62ed049936".to_string(),
-                aws::aws_sdk_ec2::types::InstanceType::T2Micro,
+                aws::aws_sdk_ec2::types::InstanceType::T2Micro.to_string(),
                 "oct-cli".to_string(),
             )
             .await;
 
             instance.create().await?;
 
+            // Save to state file
+            let instance_state = serde_json::to_string_pretty(&instance)?;
+            fs::write(state_file_path, instance_state)?;
+
             log::info!("Instance created: {}", instance.id.ok_or("No instance id")?);
         }
         Commands::Destroy(args) => {
-            // Load the state file
-            let mut state = State::load("state.json");
-
-            // Retrieve instance info from the state file
-            let instance_info = state
-                .get_instance("instance_id")
-                .expect("Instance not found in the state file");
-
-            let mut instance = aws::Ec2Instance::new(
-                instance_info.region.clone(),
-                instance_info.ami.clone(),
-                instance_info.instance_type.clone().into(),
-                instance_info.name.clone(),
-            )
-            .await;
-
-            instance.id = Some(instance_info.id.clone());
-            instance.arn = Some(instance_info.arn.clone());
-            instance.public_ip = Some(instance_info.public_ip.clone());
-            instance.public_dns = Some(instance_info.public_dns.clone());
+            // Load instance from state file
+            let instance_state = fs::read_to_string(state_file_path).expect("Unable to read file");
+            let mut instance: aws::Ec2Instance = serde_json::from_str(&instance_state)?;
 
             instance.destroy().await?;
 
-            log::info!("Instance destroyed");
+            println!("Instance destroyed: {instance:?}");
+
+            // Remove instance from state file
+            fs::remove_file(state_file_path).expect("Unable to remove file");
         }
     }
 
