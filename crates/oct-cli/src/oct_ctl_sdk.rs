52a57e7f12daabff57d785460b6f6434d87e9f94
minev-dev <90937832+minev-dev@users.noreply.github.com>
2025-01-15 23:16:16 -0800

    Wrap `oct_cli_sdk` functions into `Client` struct (#70)
    Refactors `oct-ctl` SDK into a client struct and adds state file path CLI arguments
    
    - Adds `--state-file-path` and `--user-state-file-path` CLI arguments
    - Creates `Client` struct for `oct-ctl` SDK to handle HTTP requests
    - Improves error handling for missing public IP
    - Removes hardcoded state file paths

--- crates/oct-cli/src/oct_ctl_sdk.rs
+++ crates/oct-cli/src/oct_ctl_sdk.rs
@@ -1,62 +1,64 @@
+/// TODO: Generate this from `oct-ctl`'s OpenAPI spec
 use reqwest::Response;
 use std::collections::HashMap;
 
-pub(crate) async fn run_container(
-    name: String,
-    image: String,
-    external_port: String,
-    internal_port: String,
+pub(crate) struct Client {
     public_ip: String,
-) -> Result<Response, reqwest::Error> {
-    let client = reqwest::Client::new();
-
-    let mut map = HashMap::new();
-    map.insert("name", name.as_str());
-    map.insert("image", image.as_str());
-    map.insert("external_port", external_port.as_str());
-    map.insert("internal_port", internal_port.as_str());
-
-    let response = client
-        .post(format!(
-            "http://{public_ip}:31888/run-container",
-            public_ip = public_ip
-        ))
-        .header("Content-Type", "application/json")
-        .header("Accept", "application/json")
-        .body(serde_json::to_string(&map).unwrap())
-        .send()
-        .await?;
-
-    if response.status().is_success() {
-        Ok(response)
-    } else {
-        response.error_for_status()
-    }
 }
 
-pub(crate) async fn remove_container(
-    name: String,
-    public_ip: String,
-) -> Result<Response, reqwest::Error> {
-    let client = reqwest::Client::new();
+impl Client {
+    pub(crate) fn new(public_ip: String) -> Self {
+        Self { public_ip }
+    }
 
-    let mut map = HashMap::new();
-    map.insert("name", name.as_str());
+    pub(crate) async fn run_container(
+        &self,
+        name: String,
+        image: String,
+        external_port: String,
+        internal_port: String,
+    ) -> Result<Response, reqwest::Error> {
+        let client = reqwest::Client::new();
 
-    let response = client
-        .post(format!(
-            "http://{public_ip}:31888/remove-container",
-            public_ip = public_ip
-        ))
-        .header("Content-Type", "application/json")
-        .header("Accept", "application/json")
-        .body(serde_json::to_string(&map).unwrap())
-        .send()
-        .await?;
+        let mut map = HashMap::new();
+        map.insert("name", name.as_str());
+        map.insert("image", image.as_str());
+        map.insert("external_port", external_port.as_str());
+        map.insert("internal_port", internal_port.as_str());
 
-    if response.status().is_success() {
-        Ok(response)
-    } else {
-        response.error_for_status()
+        let response = client
+            .post(format!("http://{}:31888/run-container", self.public_ip))
+            .header("Content-Type", "application/json")
+            .header("Accept", "application/json")
+            .body(serde_json::to_string(&map).unwrap())
+            .send()
+            .await?;
+
+        if response.status().is_success() {
+            Ok(response)
+        } else {
+            response.error_for_status()
+        }
+    }
+
+    pub(crate) async fn remove_container(&self, name: String) -> Result<Response, reqwest::Error> {
+        let client = reqwest::Client::new();
+
+        let mut map = HashMap::new();
+        map.insert("name", name.as_str());
+
+        let response = client
+            .post(format!("http://{}:31888/remove-container", self.public_ip))
+            .header("Content-Type", "application/json")
+            .header("Accept", "application/json")
+            .body(serde_json::to_string(&map).unwrap())
+            .send()
+            .await?;
+
+        if response.status().is_success() {
+            Ok(response)
+        } else {
+            response.error_for_status()
+        }
     }
 }
