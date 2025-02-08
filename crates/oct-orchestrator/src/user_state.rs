12347cb094aa2ed70062259bfa2e93212b20af7a
minev-dev <90937832+minev-dev@users.noreply.github.com>
2025-02-07 16:28:21 -0800

    Update `oct.toml` and user state specs (#187)

--- crates/oct-orchestrator/src/user_state.rs
+++ crates/oct-orchestrator/src/user_state.rs
@@ -1,14 +1,15 @@
+use std::collections::HashMap;
+
 use serde::{Deserialize, Serialize};
 
 #[derive(Serialize, Deserialize, Debug)]
 pub(crate) struct Service {
-    pub name: String,
     pub public_ip: String,
 }
 
 #[derive(Serialize, Deserialize, Debug, Default)]
 pub(crate) struct UserState {
-    pub services: Vec<Service>,
+    pub services: HashMap<String, Service>,
 }
 
 #[cfg(test)]
@@ -18,21 +19,23 @@ mod tests {
     #[test]
     fn test_user_state() {
         let user_state = UserState {
-            services: vec![
-                Service {
-                    name: "test".to_string(),
-                    public_ip: "test".to_string(),
-                },
-                Service {
-                    name: "test2".to_string(),
-                    public_ip: "test2".to_string(),
-                },
-            ],
+            services: HashMap::from([
+                (
+                    "test".to_string(),
+                    Service {
+                        public_ip: "test".to_string(),
+                    },
+                ),
+                (
+                    "test2".to_string(),
+                    Service {
+                        public_ip: "test2".to_string(),
+                    },
+                ),
+            ]),
         };
         assert_eq!(user_state.services.len(), 2);
-        assert_eq!(user_state.services[0].name, "test");
-        assert_eq!(user_state.services[0].public_ip, "test");
-        assert_eq!(user_state.services[1].name, "test2");
-        assert_eq!(user_state.services[1].public_ip, "test2");
+        assert_eq!(user_state.services["test"].public_ip, "test");
+        assert_eq!(user_state.services["test2"].public_ip, "test2");
     }
 }
