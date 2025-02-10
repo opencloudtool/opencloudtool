4ba60b19fe0d00dfe52186f4bfb5773ddb9397f1
minev-dev <90937832+minev-dev@users.noreply.github.com>
2025-02-09 16:37:39 -0800

    Add `new` and `save` methods to `UserState` + tests (#200)
    Related to #199

--- crates/oct-orchestrator/src/user_state.rs
+++ crates/oct-orchestrator/src/user_state.rs
@@ -1,4 +1,5 @@
 use std::collections::HashMap;
+use std::fs;
 
 use serde::{Deserialize, Serialize};
 
@@ -12,8 +13,31 @@ pub(crate) struct UserState {
     pub services: HashMap<String, Service>,
 }
 
+impl UserState {
+    /// Load state from file or create a new one
+    pub(crate) fn new(file_path: &str) -> Result<Self, Box<dyn std::error::Error>> {
+        if std::path::Path::new(file_path).exists() {
+            let existing_data = fs::read_to_string(file_path)?;
+            Ok(serde_json::from_str::<UserState>(&existing_data)?)
+        } else {
+            Ok(UserState::default())
+        }
+    }
+
+    /// Save state to file
+    pub(crate) fn save(&self, file_path: &str) -> Result<(), Box<dyn std::error::Error>> {
+        fs::write(file_path, serde_json::to_string_pretty(self)?)?;
+
+        Ok(())
+    }
+}
+
 #[cfg(test)]
 mod tests {
+    use std::io::Write;
+
+    use tempfile;
+
     use super::*;
 
     #[test]
@@ -37,5 +61,74 @@ mod tests {
         assert_eq!(user_state.services.len(), 2);
         assert_eq!(user_state.services["test"].public_ip, "test");
         assert_eq!(user_state.services["test2"].public_ip, "test2");
+    }
+
+    #[test]
+    fn test_user_state_new_exists() {
+        // Arrange
+        let state_file_content = r#"
+{
+    "services": {
+        "test": {
+            "public_ip": "test"
+        },
+        "test2": {
+            "public_ip": "test2"
+        }
+    }
+}
+"#;
+        let mut file = tempfile::NamedTempFile::new().unwrap();
+        file.write_all(state_file_content.as_bytes()).unwrap();
+
+        // Act
+        let user_state = UserState::new(file.path().to_str().unwrap()).unwrap();
+
+        // Assert
+        assert_eq!(user_state.services.len(), 2);
+        assert_eq!(user_state.services["test"].public_ip, "test");
+        assert_eq!(user_state.services["test2"].public_ip, "test2");
+    }
+
+    #[test]
+    fn test_user_state_new_not_exists() {
+        // Act
+        let user_state = UserState::new("NO_FILE").unwrap();
+
+        // Assert
+        assert_eq!(user_state.services.len(), 0);
+    }
+
+    #[test]
+    fn test_user_state_save() {
+        // Arrange
+        let user_state = UserState {
+            services: HashMap::from([(
+                "test".to_string(),
+                Service {
+                    public_ip: "test".to_string(),
+                },
+            )]),
+        };
+
+        let state_file = tempfile::NamedTempFile::new().unwrap();
+
+        // Act
+        user_state
+            .save(state_file.path().to_str().unwrap())
+            .unwrap();
+
+        // Assert
+        let file_content = fs::read_to_string(state_file.path()).unwrap();
+        assert_eq!(
+            file_content,
+            r#"{
+  "services": {
+    "test": {
+      "public_ip": "test"
+    }
+  }
+}"#
+        );
     }
 }
