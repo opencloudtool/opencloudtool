f8abb92768d900947a527c5f138f5e9396b8957f
Vladimir Minev <minev.dev@gmail.com>
2025-03-31 21:44:18 -0700

    Update `install.sh` to fail immediately if error occurs

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,10 +1,12 @@
 #!/bin/sh
 
+set -e
+
 echo "Installing oct-ctl to $HOME/.local/bin"
 
 curl \
     --output $HOME/.local/bin/oct-ctl \
-    -L \
+    -fsSL \
     https://github.com/opencloudtool/opencloudtool/releases/download/tip/oct-ctl \
     && chmod +x $HOME/.local/bin/oct-ctl
 
