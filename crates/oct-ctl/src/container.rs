f5d68212b47731107f35b95282850300f19d493e
Vladimir Minev <minev.dev@gmail.com>
2026-01-17 23:49:56 -0800

    Update `apply` and `destroy` cli functions to call `oct-ctl` (#548)
    - Update `OrchestratorWithGraph::genesis` to get the Leader node
    instance type using the user services config (`oct.toml`)
    - Simplify `OrchestratorWithGraph::apply` to bypass config to `apply`
    endpoint, the Leader node takes care of the proper service-by-service
    deployment
    - Simplify `OrchestratorWithGraph::destroy` to not manage user services
    state, it's done on the Leader node
    - Replace separate `oct-ctl` methods to run/remove containers by `apply`
    method that accept the expected user services.
    - `destroy` method of `oct-ctl` only reads the user state but doesn't
    remove the services because it requires additional logic to convert
    state to a graph and properly remove it. This logic will be implemented
    in the next version, for now it's okay to just remove the instance with
    all running services.
    - Remove functions to build/push OCI images from
    `crates/oct-orchestrator/src/lib.rs`
    
    Closes #545

--- crates/oct-ctl/src/container.rs
+++ crates/oct-ctl/src/container.rs
@@ -80,6 +80,9 @@ impl ContainerEngine {
     }
 
     /// Removes container
+    ///
+    /// Temporarily marked as unused until container destruction logic is implemented
+    #[allow(dead_code)]
     pub(crate) fn remove(&self, name: &str) -> Result<(), Box<dyn std::error::Error>> {
         let output = self
             .executor
